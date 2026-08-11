07cc035a19edfe2cc2007352f405995f103d8ea4
Nicholas-Nak <nakonechnyi.mykola.od@gmail.com>
2026-08-11 15:43:07 +0000

    Add HTML structure for header and sidebar

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
 
   module: {
     rules: [
+        {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
