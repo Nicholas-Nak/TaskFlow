70034222e234dbd4b9b8335368fe41758d436f17
Nicholas-Nak <nakonechnyi.mykola.od@gmail.com>
2026-08-12 21:28:09 +0000

    Add localStorage logic

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
-import './styles.css';
-import { renderCurrentTime } from './time';
-import { renderProjectForm } from './addProjectForm';
+import "./styles.css";
+import { renderCurrentTime } from "./time";
+import { renderProjectForm } from "./addProjectForm";
+import { Projects, Project, Todo } from "./classes";
+
 renderCurrentTime();
+let appProjects = new Projects();
 
-function clearMain(){
-    const main = document.getElementById("dynamic-content");
-    main.innerHTML = '';
+export function saveStorage() {
+  localStorage.setItem("todoList_data", JSON.stringify(appProjects));
 }
-const Projects = new Projects();
-const firstProject = new Project("Базовий");
-Projects.addProject(firstProject);
+function loadStorage() {
+  const data = localStorage.getItem("todoList_data");
 
-const newProject = document.getElementById("New-project");
-newProject.addEventListener("click", ()=>{
-    renderProjectForm();
-    new
+  if (data) {
+    const parsedData = JSON.parse(data);
+    parsedData.projects.forEach((projData) => {
+      const project = new Project(projData.title, projData.id);
+      projData.tasks.forEach((taskData) => {
+        const task = new Todo(taskData.title, taskData.id, taskData.status);
+        project.addTask(task);
+      });
+
+      appProjects.addProject(project);
+    });
+  } else {
+    const firstProject = new Project("Базовий");
+    appProjects.addProject(firstProject);
+    saveStorage();
+  }
+}
+loadStorage();
+
+function clearMain() {
+  const main = document.getElementById("dynamic-content");
+  main.innerHTML = "";
+}
+const newProjectBtn = document.getElementById("New-project");
+newProjectBtn.addEventListener("click", () => {
+  renderProjectForm();
 });
