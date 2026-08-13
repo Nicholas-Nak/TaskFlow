4b6d38617e5068100cc5e43e91464c84e7903bd6
Nicholas-Nak <nakonechnyi.mykola.od@gmail.com>
2026-08-13 13:24:54 +0000

    Add delete project logic

--- src/ui/renderProjects.js
+++ src/ui/renderProjects.js
@@ -1,18 +1,21 @@
-import { loadStorage } from "../logic/storage";
+
 import "../logic/classes"
 import closeIconPath from "../images/free-icon-close-1828747.png";
 import editIconPath from "../images/pen.png"
+import { loadStorage, saveStorage } from "../logic/storage";
  
-export function renderProjects(){
-   const  allProjects = loadStorage();
-   const projectsContainer = document.getElementById('projects-container');
-   projectsContainer.innerHTML = '';
-   allProjects.getProjects().forEach((project)=>{ 
-    const  projContainer = document.createElement('div');
+export function renderProjects() {
+  const allProjects = loadStorage();
+  const projectsContainer = document.getElementById('projects-container');
+  
+  projectsContainer.innerHTML = '';
+
+  allProjects.getProjects().forEach((project) => {
+    const projContainer = document.createElement('div');
     projContainer.classList.add('project');
     
     const projBtn = document.createElement('button');
-    projBtn.id ='inbox';
+    projBtn.classList.add('project-inbox-btn'); 
     
     const projectNameContainer = document.createElement('div');
     projectNameContainer.classList.add('project-name-container');
@@ -24,40 +27,40 @@ export function renderProjects(){
     projectTitle.textContent = project.getTitle();
 
     const activeProjects = document.createElement('span');
-    activeProjects.id = "quantity-of-inbox";
-    activeProjects.textContent = '('+project.getActive()+')';
+    activeProjects.classList.add("quantity-of-inbox"); 
+    activeProjects.textContent = '(' + project.getActive() + ')';
     
     const editBtn = document.createElement('button');
-    editBtn.id = 'icon-edit-btn';
-    editBtn.classList.add("icon-btn");
+    editBtn.classList.add("icon-btn", "edit-btn"); 
     
     const editImg = document.createElement('img');
-    editImg.id = 'pen-icon';
     editImg.setAttribute("src", editIconPath);
-    editBtn.setAttribute("alt","pen");
+    editBtn.setAttribute("alt", "pen");
     
     const deleteBtn = document.createElement('button');
-    deleteBtn.id = "icon-delete-btn";
-    deleteBtn.classList.add('icon-btn');
+    deleteBtn.classList.add('icon-btn', 'delete-btn'); 
 
     const deleteImg = document.createElement('img');
-    deleteImg.id = 'close-icon';
-    deleteImg.setAttribute("src",closeIconPath);
-    deleteBtn.setAttribute("alt","close");
+    deleteImg.setAttribute("src", closeIconPath);
+    deleteBtn.setAttribute("alt", "close");
 
-    
-     
     projectsContainer.appendChild(projContainer);
     projContainer.appendChild(projBtn);
     projBtn.appendChild(projectNameContainer);
     projectNameContainer.appendChild(circleStatus);
     projectNameContainer.appendChild(projectTitle);
+    projectNameContainer.appendChild(activeProjects); 
+    
     projContainer.appendChild(editBtn);
     editBtn.appendChild(editImg);
+    
     projContainer.appendChild(deleteBtn);
     deleteBtn.appendChild(deleteImg);
 
-   });
-   
-
+    deleteBtn.addEventListener('click', () => {
+      allProjects.deleteProject(project);
+      saveStorage(allProjects);
+      renderProjects(); 
+    });
+  });
 }
